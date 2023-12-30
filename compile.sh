15ff4cc9f7fe21899683e8e6fba3d87606ec735b
EPICGameGuy <epicgameguy@cactus.vg>
2023-12-30 01:00:12 -0500

    Adding ps2stuff to compile script

--- compile.sh
+++ compile.sh
@@ -28,6 +28,14 @@ pushd dependencies/openvcl
 sudo -E make install -j$(nproc)
 popd
 
+# PS2Stuff
+if [ ! -d "dependencies/ps2stuff" ]; then
+    echo "------Cloning ps2stuff------"
+    git clone https://github.com/ps2dev/ps2stuff.git dependencies/ps2stuff
+fi
+echo "------Compiling ps2gl------"
+pushd dependencies/ps2stuff && make install -j$(nproc); popd
+
 # PS2GL
 if [ ! -d "dependencies/ps2gl" ]; then
     echo "------Cloning ps2gl------"
