927bf6805428ca4d9f84ebc26717e0fb1a269d61
EPICGameGuy <epicgameguy@cactus.vg>
2023-12-30 01:09:59 -0500

    Changing docker workflow to use compile script

--- compile.sh
+++ compile.sh
@@ -2,6 +2,32 @@
 
 # sudo apt install build-essential binutils-dev git cmake genisoimage libassimp-dev 
 
+# Meshoptimizer
+if [ ! -d "dependencies/meshoptimizer" ]; then
+    echo "------Cloning meshoptimizer------"
+    git clone https://github.com/zeux/meshoptimizer.git dependencies/meshoptimizer
+fi
+echo "------Compiling meshoptimizer------"
+pushd dependencies/meshoptimizer
+mkdir -p build
+cd build
+cmake ..
+sudo make install -j$(nproc)
+popd
+
+# cxxopts
+if [ ! -d "dependencies/cxxopts" ]; then
+    echo "------Cloning cxxopts------"
+    git clone https://github.com/artpaul/cxxopts.git dependencies/cxxopts
+fi
+echo "------Compiling cxxopts------"
+pushd dependencies/cxxopts
+mkdir -p build
+cd build
+cmake ..
+sudo make install -j$(nproc)
+popd
+
 echo "------Compiling egg-library------"
 pushd dependencies/egg-library && ./compile.sh; popd
 echo "------Compiling ps2-manifest-generator------"
@@ -18,7 +44,7 @@ echo "------Compiling Masp------"
 pushd dependencies/openvcl/contrib/masp
 if [ ! -f "Makefile" ]; then
     chmod +x ./configure
-    ./configure
+    ./configure LIBS="-lobstack"
 fi
 sudo -E make install -j$(nproc)
 popd
