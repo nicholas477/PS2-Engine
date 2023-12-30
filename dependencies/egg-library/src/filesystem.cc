8fa8ffc989a23849ac21eaac035693379db550db
EPICGameGuy <epicgameguy@cactus.vg>
2023-12-30 14:51:29 -0500

    Trying to fix filesystem issue with CI build

--- dependencies/egg-library/src/filesystem.cc
+++ dependencies/egg-library/src/filesystem.cc
@@ -26,8 +26,8 @@ void set_filesystem_type(Type new_type)
 bool load_file(const Path& path, std::vector<std::byte>& out_bytes)
 {
 	const char* filepath = path.to_full_filepath();
-	std::ifstream file(filepath, std::ios::binary);
 	printf("Filesystem::load_file: %s\n", filepath);
+	std::ifstream file(filepath, std::ios::binary);
 	if (file.is_open() && file.good())
 	{
 		// Read the file size
@@ -48,8 +48,8 @@ bool load_file(const Path& path, std::vector<std::byte>& out_bytes)
 bool load_file(const Path& path, std::unique_ptr<std::byte[]>& out_bytes, size_t& size, size_t alignment)
 {
 	const char* filepath = path.to_full_filepath();
-	std::ifstream file(filepath, std::ios::binary);
 	printf("Filesystem::load_file: %s\n", filepath);
+	std::ifstream file(filepath, std::ios::binary);
 	if (file.is_open() && file.good())
 	{
 		// Read the file size
